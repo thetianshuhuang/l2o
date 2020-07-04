28d2c1d0bb366f6c52bfe06dea272ad97749dbb3
thetianshuhuang <thetianshuhuang@gmail.com>
2020-07-04 13:59:07 -0500

    Added test for same-problem

--- test.py
+++ test.py
@@ -9,3 +9,10 @@ opt = CoordinateWiseOptimizer(net)
 
 train(
     opt, problems, tf.keras.optimizers.Adam(), repeat=10)
+
+
+test = Quadratic(20)
+print(test.objective(None))
+for _ in range(20):
+    opt.minimize(test.objective(None))
+print(test.objective(None))
