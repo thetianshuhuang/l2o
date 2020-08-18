e1576cd366fc8913ccedab3c3a3fc8b545012076
thetianshuhuang <thetianshuhuang@gmail.com>
2020-08-17 22:22:48 -0500

    Moved period parsing inside loop

--- evaluate.py
+++ evaluate.py
@@ -9,16 +9,18 @@ period = sys.argv[3]
 
 strategy = l2o.train.build_from_config(folder)
 
-if period == '*':
-    periods = list(strategy._filter(stage=int(stage))["period"])
-else:
-    periods = [int(period)]
 
 if stage == '*':
     stages = list(strategy.summary["stage"].unique())
 else:
     stages = [int(stage)]
 
+
 for stage in stages:
+    if period == '*':
+        periods = list(strategy._filter(stage=stage)["period"])
+    else:
+        periods = [int(period)]
+
     for period in periods:
         strategy.evaluate(stage, period)
