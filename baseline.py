9a444680bacd0c8e3ddb68a21f5fb85b999f3a59
thetianshuhuang <thetianshuhuang@gmail.com>
2020-08-25 13:32:19 -0500

    Updated baseline

--- baseline.py
+++ baseline.py
@@ -1,8 +1,11 @@
+"""Adam Baseline."""
+
 import json
 import tensorflow as tf
 import l2o
 
 
-results = l2o.evaluate.evaluate(tf.keras.optimizers.Adam())
-with open("baseline.json", "w") as f:
-    json.dump(results, f)
+results = [
+    l2o.evaluate.evaluate(tf.keras.optimizers.Adam()) for _ in range(10)]
+results = {k: np.stack([d[k] for d in results]) for k in results[0]}
+np.savez('baseline_adam.npz', **results)
