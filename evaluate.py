efbef541d506626a72c010918f58dec9fa317869
thetianshuhuang <thetianshuhuang@gmail.com>
2020-12-24 22:02:29 -0600

    Updated eval cmd args

--- evaluate.py
+++ evaluate.py
@@ -2,13 +2,12 @@
 
 Run with
 ```
-python resume.py directory --vgpu=1
+python evaluate.py directory --vgpu=1
 ```
 """
 
 import os
 import sys
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
@@ -16,12 +15,16 @@ import l2o
 from config import create_distribute, ArgParser, get_eval_problem
 
 
-args = ArgParser(sys.argv[2:])
+args = ArgParser(sys.argv[1:])
 vgpus = int(args.pop_get("--vgpu", default=1))
 distribute = create_distribute(vgpus=vgpus)
 
+problem = args.pop_get("--problem", "conv_train")
+target = args.pop_get("--directory", "weights")
+output = args.pop_get("--out", "eval")
+
 with distribute.scope():
-    strategy = l2o.strategy.build_from_config(sys.argv[1])
+    strategy = l2o.strategy.build_from_config(directory)
     config = get_eval_problem("debug")
     strategy.evaluate(
-        metadata={"period": 1}, repeat=1, file="test", **config)
+        metadata={"period": 99}, repeat=10, file=output, **config)
