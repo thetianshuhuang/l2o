Indexed wrong dimension in hard max

l2o/policies/choice.py
@@ -86,8 +86,8 @@ class ChoiceOptimizer(BaseCoordinateWisePolicy):
             # Otherwise, use ordinary softmax.
             else:
                 opt_weights = tf.cast(tf.stack([
-                    opt_weights[0] > opt_weights[1],
-                    opt_weights[0] < opt_weights[1]], tf.float32))
+                    opt_weights[:, 0] > opt_weights[:, 1],
+                    opt_weights[:, 0] < opt_weights[:, 1]], tf.float32))
         # Soft Choice
         else:
             # Manual softmax in order to add epsilon in denominator