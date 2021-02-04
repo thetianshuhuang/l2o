Fixed softmax bug - no exp in numerator; added last repeat logic to RepeatStrategy evaluation.

l2o/evaluate/models.py
@@ -32,7 +32,7 @@ def conv_classifier(
 
     if head_type == "dense":
         return tf.keras.Sequential(
-            [tf.keras.layers.Input(shape=info.features['image'].shape)] 
+            [tf.keras.layers.Input(shape=info.features['image'].shape)]
             + [_deserialize(x) for x in layers]
             + [tf.keras.layers.Flatten()]
             + [tf.keras.layers.Dense(

l2o/policies/softmax.py
@@ -35,5 +35,6 @@ def softmax(weights, hardness=0.0, train=True, epsilon=1e-10):
             return tf.one_hot(tf.math.argmax(weights, axis=1), dim)
     # Soft Choice
     else:
-        return weights / (
-            tf.reduce_sum(tf.exp(weights), axis=1, keepdims=True) + epsilon)
+        weights_exp = tf.exp(weights)
+        return weights_exp / (
+            tf.reduce_sum(weights_exp, axis=1, keepdims=True) + epsilon)

l2o/train/step_callbacks.py
@@ -8,15 +8,13 @@ class BaseStepCallback:
 
     Parameters
     ----------
-    keys : dict
-        Keys are the names of statistic names that will be logged.
-        Values are callables that reduce a list of values to a single value:
-        np.stack, np.mean, np.sum, etc.
+    parent : train.OptimizerTraining
+        Parent training class.
     """
 
     keys = {}
 
-    def __init__(self, unroll, n_teachers):
+    def __init__(self, parent):
         pass
 
     def on_step_end(self, index, current_obj, teacher_loss):