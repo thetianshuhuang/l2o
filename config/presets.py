8f2f1593d64a1d78aa7f2eb81a13f7a59dd30165
thetianshuhuang <thetianshuhuang@gmail.com>
2021-03-04 21:40:11 -0600

    added long preset without warmup

--- config/presets.py
+++ config/presets.py
@@ -92,7 +92,7 @@ OVERRIDE_PRESETS = {
         (["training", "scale_objective"], False),
         (["strategy", "annealing_schedule", "value"], 0.25)
     ],
-    "long": [
+    "long-warmup": [
         (["strategy", "unroll_len"], 100),
         (["strategy", "depth"], 5),
         (["strategy", "validation_unroll"], 100),
@@ -101,6 +101,12 @@ OVERRIDE_PRESETS = {
         (["strategy", "warmup_rate"], {"type": "list", "values": [0.0, 0.05]}),
         (["strategy", "validation_warmup"], 1),
         (["strategy", "validation_warmup_rate"], 0.05)
+    ],
+    "long": [
+        (["strategy", "unroll_len"], 100),
+        (["strategy", "depth"], 5),
+        (["strategy", "validation_unroll"], 100),
+        (["strategy", "validation_depth"], 5),
     ],
 }
 
