3375b21efe00064b77f88c9c5da61f78fd3cdfaf
thetianshuhuang <thetianshuhuang@gmail.com>
2021-03-21 22:13:53 -0500

    Added cl_test with no hidden state update during warmup

--- config/presets.py
+++ config/presets.py
@@ -206,6 +206,25 @@ OVERRIDE_PRESETS = {
             "name": "CurriculumLearningStrategy"
         }
     )],
+    "cl_test": [(
+        ["strategy"], {
+            "validation_problems": None,
+            "validation_seed": 12345,
+            "num_stages": 3,
+            "num_periods": 2,
+            "num_chances": 3,
+            "unroll_len": 100,
+            "depth": {"type": "list", "values": [1, 2, 5, 10]},
+            "epochs": 10,
+            "annealing_schedule": 0.0,
+            "validation_epochs": 10,
+            "max_repeat": 2,
+            "repeat_threshold": 0.8,
+            "warmup": 1,
+            "warmup_rate": 0.05,
+            "name": "CurriculumLearningStrategy"
+        }
+    )],
     "il_scaled": [
         (["strategy", "annealing_schedule"],
          {"type": "list", "values": [0.1, 0.05, 0.01]}),
