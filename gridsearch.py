1d1f1b46d4030b52311774404d6bbade49683359
thetianshuhuang <thetianshuhuang@gmail.com>
2021-04-19 21:57:42 -0500

    Updated imports

--- gridsearch.py
+++ gridsearch.py
@@ -7,7 +7,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
 import l2o
-from config import create_distribute, ArgParser, get_eval_problem
+from config import ArgParser, get_eval_problem
+from gpu_setup import create_distribute
+
 
 args = ArgParser(sys.argv[1:])
 vgpus = int(args.pop_get("--vgpu", default=1))
