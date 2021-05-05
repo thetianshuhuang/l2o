d5794d2bb186565fb8fa9b32a8cbd653ec8d1786
thetianshuhuang <thetianshuhuang@gmail.com>
2021-05-04 19:32:19 -0500

    Added proper progbar to fit_dp

--- l2o/evaluate/fit_dp.py
+++ l2o/evaluate/fit_dp.py
@@ -145,7 +145,8 @@ def model_dp_fit(
         log_debug = None
 
     # Epoch loop
-    for _ in tqdm(range(epochs), desc=desc):
+    pbar = Progbar(epochs, unit_name='epoch')
+    for _ in range(epochs):
         train_loss, train_time, train_metrics = run_loop(
             train, train_step, callback=log_debug)
         stats["batch_loss"] += train_loss
@@ -159,6 +160,9 @@ def model_dp_fit(
         stats["val_loss"].append(np.mean(test_loss))
         for m, val in zip(metrics, test_metrics):
             stats["val_" + m.name].append(val)
+
+        pbar.add(1, values=[
+            ("train", stats["loss"][-1]), ("val", stats["val_loss"][-1])])
 
     res = {k: np.array(v, dtype=np.float32) for k, v in stats.items()}
     if debug:
