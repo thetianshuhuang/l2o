85c821851d1c1daa6e97ae2c69ead094c279dfa6
thetianshuhuang <thetianshuhuang@gmail.com>
2021-05-11 22:16:58 -0500

    Typo: list->get because tensorflow is inconsistent like that

--- gpu_setup.py
+++ gpu_setup.py
@@ -17,7 +17,7 @@ def create_distribute(
         for gpu in gpus.split(','):
             dev_list += gpu_list[int(gpu)]
 
-    gpus = tf.config.experimental.list_visible_devices('GPU')
+    gpus = tf.config.get_visible_devices('GPU')
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
 
